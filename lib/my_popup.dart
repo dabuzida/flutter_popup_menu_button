076f070aba96ca7a5f3d5be4e023e4db45ee4e97
dabuzida <dabuzida@gmail.com>
2023-06-14 10:40:24 +0900

    test

--- lib/my_popup.dart
+++ lib/my_popup.dart
@@ -8,8 +8,45 @@ class MyPopup extends StatefulWidget {
 }
 
 class _MyPopupState extends State<MyPopup> {
+  int _num = 0;
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Center(
+      child: PopupMenuButton<int>(
+        initialValue: _num,
+        // Callback that sets the selected popup menu item.
+        onSelected: (int num) {
+          _num = num;
+          setState(() {});
+        },
+        itemBuilder: (BuildContext context) {
+          List<PopupMenuEntry<int>> list = <PopupMenuEntry<int>>[];
+
+          final List<int> xx = <int>[2, 5, 2, 6, 34, 756, 1];
+          xx.sort((a, b) => a.compareTo(b));
+
+          for (int i = 0; i < xx.length; ++i) {
+            list.add(
+              PopupMenuItem<int>(
+                value: xx[i],
+                child: Text(
+                  xx[i].toString(),
+                ),
+              ),
+            );
+          }
+
+          return list;
+        },
+        child: Container(
+          width: 100,
+          height: 50,
+          color: Colors.red,
+          alignment: Alignment.center,
+          child: Text(_num.toString()),
+        ),
+      ),
+    );
   }
 }
